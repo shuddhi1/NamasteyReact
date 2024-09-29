78ff38a929d9a3210de9b3d4bcf332ae02fc2d7d
Shuddhi1 <siddharthnaithani@hotmail.com>
2024-09-29 15:18:30 +0530

    inception

---
+++ App.js
@@ -0,0 +1,13 @@
+
+
+
+    const heading = React.createElement("h1",
+     
+        {
+            id:"heading",
+            xyz:"abc"
+          
+        },"Hello world from react!");
+      const root = ReactDOM.createRoot(document.getElementById("root"));
+       root.render(heading);
+    
